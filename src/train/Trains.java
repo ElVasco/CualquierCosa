8d6ee09879fa9e59a4aaa77550e534622485aa9d
ElVasco <txomin.gutierrez@gmail.com>
2017-06-13 20:38:09 +0200

    Adding trains/java to prove code

---
+++ src/train/Trains.java
@@ -0,0 +1,19 @@
+package trains;
+import trains.Graph;
+
+
+public class Trains {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String file = "data/graphs/example1.csv";
+		Graph G = new Graph(file);
+		G.readGraph(file);
+		System.out.println(G.distance("A-B-C"));
+		System.out.println(G.distance("A-D"));
+		System.out.println(G.distance("A-D-C"));
+		System.out.println(G.distance("A-E-B-C-D"));
+		System.out.println(G.distance("A-E-D"));
+	}
+
+}
